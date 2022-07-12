5e558de1b72f837f2aff4fc86f0b2e90633af081
Ismail Azdad <ismail.azdad@adventori.com>
2022-07-12 18:26:49 +0200

    remove usless url

--- js/world.js
+++ js/world.js
@@ -15,7 +15,7 @@
 		// Initialize the map
 		map = L.map( 'map' ).setView( [0,0], 2 );
 		L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: 'location-history-visualizer is open source and available <a href="https://github.com/theopolisme/location-history-visualizer">on GitHub</a>. Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors.',
+			attribution: ' map with <a href="https://openstreetmap.org">OpenStreetMap</a>',
 			maxZoom: 18,
 			minZoom: 2
 		} ).addTo( map );
